f77978cfc47c1af9d8b681259b8580980295a20c
Z <60660437+ZacharyWills@users.noreply.github.com>
2020-10-10 00:20:45 -0500

    Update Print_NOAA_GH.sh
    lengthen and add JQ example in comments

--- Print_NOAA_GH.sh
+++ Print_NOAA_GH.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gh_search=$(curl -s "https://api.github.com/search/users?q=NOAA-")
+gh_search=$(curl -s "https://api.github.com/search/users?q=NOAA-&per_page=200")
 TOTAL=$(echo "$gh_search" | grep total | awk '{ print $2 }' | sed 's/,*$//g')
 USERS=$(echo "$gh_search" | grep login | awk '{ print $2 }' | sort | tr -d '"' | sed 's/,*$//g')
 
@@ -10,8 +10,10 @@ echo "There are $TOTAL NOAA github Orgs:"
 for i in $USERS 
 do
     echo "$i"
-    curl -s https://api.github.com/orgs/$i/repos | grep full_name | awk '{ print $2 }' | sed 's/,*$//g'
+    curl -s https://api.github.com/orgs/$i/repos?per_page=200 | grep full_name | awk '{ print $2 }' | sed 's/,*$//g'
+    #jq --raw-output .[].full_name will avoid the bash mess if you have jq
 done
-#curl https://api.github.com/orgs/noaa-owp/repos | grep full_name | awk '{ print $2 }'
+#curl https://api.github.com/orgs/noaa-owp/repos?per_page=200 | grep full_name | awk '{ print $2 }'
 #curl https://api.github.com/orgs/noaa-edab/repos | grep full_name | awk '{ print $2 }'
+#curl https://www.fire.ca.gov/ubraco/Api/IncidentApi/GetIncidents | jq --raw-output '.Incidents | .[].Name'
 exit
