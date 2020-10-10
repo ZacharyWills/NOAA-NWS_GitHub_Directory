7098a8b117d2236d9de270c423bc3ea12c77581d
Z <60660437+ZacharyWills@users.noreply.github.com>
2020-10-10 00:31:09 -0500

    Add scrape example

--- Print_NOAA_GH.sh
+++ Print_NOAA_GH.sh
@@ -10,7 +10,7 @@ echo "There are $TOTAL NOAA github Orgs:"
 for i in $USERS 
 do
     echo "$i"
-    curl -s https://api.github.com/orgs/$i/repos?per_page=200 | grep full_name | awk '{ print $2 }' | sed 's/,*$//g'
+    curl -s https://api.github.com/orgs/$i/repos?per_page=200 | grep full_name | awk '{ print $2 }' | sort | tr -d '"' | sed 's/,*$//g'
     #jq --raw-output .[].full_name will avoid the bash mess if you have jq
 done
 #curl https://api.github.com/orgs/noaa-owp/repos?per_page=200 | grep full_name | awk '{ print $2 }'
