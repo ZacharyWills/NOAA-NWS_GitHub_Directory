0fe2d4e253958b9d1e4abcc97b18cabb6e2245cd
Z <60660437+ZacharyWills@users.noreply.github.com>
2020-10-11 11:40:11 -0500

    Markdown Generation
    Added script for markdown output, loops through the API for total number of orgs and loops through the list of orgs for their repos, putting them in an expandable section for interaction. Has formatting issues, but nothing major.

---
+++ Print_NOAA_GH_MD.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+gh_search=$(curl -s "https://api.github.com/search/users?q=NOAA-&per_page=200")
+TOTAL=$(echo "$gh_search" | grep total | awk '{ print $2 }' | sed 's/,*$//g')
+USERS=$(echo "$gh_search" | grep login | awk '{ print $2 }' | sort | tr -d '"' | sed 's/,*$//g')
+
+echo "# There are $TOTAL NOAA github Orgs:"
+#search for NOAA- users
+#echo "$USERS"
+#return repo names for an org, to be specified later
+for i in $USERS 
+do
+    echo "## $i"
+    echo "<details>"
+    echo "<summary>Click to $i Repositories</summary>"
+    #this command gets the repos for the orgs and places them into markdown below the org 
+    curl -s https://api.github.com/orgs/$i/repos?per_page=200 | grep full_name | awk '{ print "* "$2"\n" }' | sort | tr -d '"' | sed 's/,*$//g'
+    #jq --raw-output .[].full_name will avoid the bash mess if you have jq
+    echo "</details>"
+    echo -en "\n"
+done
+#curl https://api.github.com/orgs/noaa-owp/repos?per_page=200 | grep full_name | awk '{ print $2 }'
+#curl https://api.github.com/orgs/noaa-edab/repos | grep full_name | awk '{ print $2 }'
+#curl https://www.fire.ca.gov/ubraco/Api/IncidentApi/GetIncidents | jq --raw-output '.Incidents | .[].Name'
+exit
